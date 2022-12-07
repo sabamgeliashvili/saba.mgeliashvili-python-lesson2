3b4321ee1302927598183b4f2c7f687351a3dfc1
sabamgeliashvili <sba69717@gmail.com>
2022-12-07 13:54:36 +0400

    day 3 commit

--- day2/lesson2.py
+++ day2/lesson2.py
@@ -12,30 +12,3 @@ print(full_name[1:4])
 print(full_name[1:])
 
  
-num1 = int(input("enter first number "))
-num2 = int(input("enter second number "))
-multiply = num1 * num2
-
-if multiply > 100:
-    print(multiply)
-else:
-    print('you lose')
-
-
-
-
-#  დავალება
-
-num1 = int(input("enter first number "))
-num2 = int(input("enter second number "))
-num3 = int(input("enter third number "))
-sum = num1 + num2 + num3
-
-if num1 % 2 == 0:
-    print('error')
-if num2 % 2 == 0:
-    print('error')
-if num3 % 2 == 0:
-    print('error')
-else:
-    print(sum)
--- day3/lesson3.py
+++ day3/lesson3.py
@@ -11,3 +11,33 @@ my_age = int(input('enter your age: '))
 # print(my_sent)
 my_age += 3
 print('after 3 years i am now {} years old'.format(my_age))
+
+
+
+num1 = int(input("enter first number "))
+num2 = int(input("enter second number "))
+multiply = num1 * num2
+
+if multiply > 100:
+    print(multiply)
+else:
+    print('you lose')
+
+
+
+
+#  დავალება
+
+num1 = int(input("enter first number "))
+num2 = int(input("enter second number "))
+num3 = int(input("enter third number "))
+sum = num1 + num2 + num3
+
+if num1 % 2 == 0:
+    print('error')
+if num2 % 2 == 0:
+    print('error')
+if num3 % 2 == 0:
+    print('error')
+else:
+    print(sum)
